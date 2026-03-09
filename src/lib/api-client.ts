6c4487feda876aa422e718262ad55dde29580ab3
iamfineboyygabriel54321 <tundegabriel999@gmail.com>
2026-03-09 12:11:32 +0100

    feat: fixes

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -26,10 +26,10 @@ apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      const isOnLoginPage = window.location.pathname === '/'
+      const isOnLoginPage = window.location.pathname === '/login'
       if (!isOnLoginPage) {
         localStorage.removeItem('auth_token')
-        window.location.href = '/'
+        window.location.href = '/login'
       }
     }
     return Promise.reject(error)
